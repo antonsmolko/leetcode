2d4097f67bc4c0f112c2544805cdbf7de2a65ab0
antonsmolko <asmolko@yandex.ru>
2023-11-26 13:19:51 +0300

    can-partition-k-subsets

---
+++ src/problems/can-partition-k-subsets/readme.md
@@ -0,0 +1,3 @@
+### Partition to K Equal Sum Subsets
+
+[leetcode](https://leetcode.com/problems/partition-to-k-equal-sum-subsets/)
---
+++ src/problems/can-partition-k-subsets/test.ts
@@ -0,0 +1,15 @@
+import { describe, expect, test } from '@jest/globals'
+import solution from '.'
+
+describe('canPartitionKSubsets', () => {
+  const matrix: [nums: number[], k: number, expected: boolean][] = [
+    [[4,3,2,3,5,2,1], 4, true],
+    [[1,2,3,4], 3, false],
+    [[4,4,6,2,3,8,10,2,10,7], 4, true],
+    [[3522,181,521,515,304,123,2512,312,922,407,146,1932,4037,2646,3871,269], 5, true]
+  ]
+
+  test.each(matrix)('should work with nums: %s, k: %s', (nums, k, expected): void => {
+    expect(solution(nums, k)).toEqual(expected)
+  })
+});
