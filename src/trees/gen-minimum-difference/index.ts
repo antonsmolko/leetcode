12b1570dc36e7956bbb9dc38e0d613f4b42874d0
antonsmolko <asmolko@yandex.ru>
2024-02-11 13:28:27 +0300

    trees: gen-minimum-difference

---
+++ src/trees/gen-minimum-difference/index.ts
@@ -0,0 +1,25 @@
+import { TreeNode } from '../utils'
+const getMinimumDifference = (root: TreeNode | null): number => {
+    let minDiff: number = Infinity
+    let prevNode: TreeNode | null = null
+
+    const setMinDiff = (node: TreeNode | null) => {
+        if (!node) return
+
+        setMinDiff(node.left)
+
+        if (prevNode) {
+            minDiff = Math.min(minDiff, Math.abs(node.val - prevNode.val))
+        }
+
+        prevNode = node
+
+        setMinDiff(node.right)
+    }
+
+    setMinDiff(root)
+
+    return minDiff
+}
+
+export default getMinimumDifference
---
+++ src/trees/gen-minimum-difference/test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, test } from '@jest/globals'
+import solution from '.'
+import { genTree, TreeNode } from '../utils'
+
+describe('trees', () => {
+    describe('getMinimumDifference', () => {
+        const matrix: [root: TreeNode | null, expected: number][] = [
+            [genTree([4,2,6,1,3]), 1],
+            [genTree([1,0,48,null,null,12,49]), 1]
+        ]
+
+        test.each(matrix)(
+            'should work',
+            (root, expected
+            ): void => {
+                expect(solution(root)).toEqual(expected)
+            })
+    })
+})
